9ba2d7d812f4af504917971dbcfd72a4654ba6a1
Meraj al Maksud <merajmasuk@gmail.com>
2022-12-18 03:45:58 +0600

    setup day2 with vscode

--- djangolessons/djangoproject/djangoproject/urls.py
+++ djangolessons/djangoproject/djangoproject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("day1.urls")),
+    path('day1', include("day1.urls")),
+    path('day2', include("day2.urls")),
 ]
