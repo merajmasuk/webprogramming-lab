a4e61c5b5a69444d21e17aa88a4398986c151c4e
Meraj al Maksud <meraj.stu2017@juniv.edu>
2023-04-13 21:41:10 +0600

    delete student-info and student-info2
    reason:files moved to another repository

--- student-info2/studentinfo/studentinfo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.Members)
-admin.site.register(models.Student)
-admin.site.register(models.Course)
